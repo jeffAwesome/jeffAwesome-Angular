6b6b3ac9e1af1c45104e40a5c20350b6da8b5093
Jeff Richardson <jeff@masterpieceinteractive.com>
2015-09-12 15:39:06 -0500

    short circuit dirty loop, more efficient

--- src/scope.js
+++ src/scope.js
@@ -3,6 +3,7 @@
 
 function Scope() {
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 }
 
 function initWatchVal() {
@@ -18,17 +19,35 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
   this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function() {
+Scope.prototype.$$digestOnce = function() {
   var self = this;
-  var newValue, oldValue;
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     if (newValue !== oldValue) {
+      self.$$lastDirtyWatch = watcher;
       watcher.last = newValue;
       watcher.listenerFn(newValue,
           (oldValue === initWatchVal ? newValue : oldValue),
           self);
-    } 
+      dirty = true;
+    } else if (self.$$lastDirtyWatch === watcher) {
+      return false;
+    }
   });
+  return dirty;
+};
+
+
+Scope.prototype.$digest = function() {
+  var ttl = 10;
+  var dirty;
+  this.$$lastDirtyWatch = null;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw "10 digest iterations reached";
+    }
+  } while (dirty);
 };
